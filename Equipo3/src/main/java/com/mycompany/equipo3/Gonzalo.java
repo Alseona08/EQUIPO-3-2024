2bd977611ff6cc104da29530f433751b4b605679
Gonzalo <gonzagar12@educastillalamancha.es>
2024-02-06 09:43:09 +0100

    06/02/2024
    Metodos Gonzalo

--- Equipo3/src/main/java/com/mycompany/equipo3/Gonzalo.java
+++ Equipo3/src/main/java/com/mycompany/equipo3/Gonzalo.java
@@ -4,10 +4,61 @@
  */
 package com.mycompany.equipo3;
 
+import java.util.Collection;
+import java.util.Iterator;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
 /**
  *
  * @author migue
  */
+
 public class Gonzalo {
+    
+        private static EntityManagerFactory emf;
+        private static EntityManager em;
+        
+    public static void listadoRegistros(){
+        Transacciones transaccion;
+        TypedQuery<Transacciones> query = em.createQuery("SELECT t from Transacciones t",Transacciones.class);
+        
+        try{
+            Collection<Transacciones> coleccion=query.getResultList();
+            Iterator<Transacciones> it=coleccion.iterator();
+            
+            System.out.print("TransaccionID \t");
+            System.out.print("LibroID_Origen \t");
+            System.out.print("LibroID_Destino \t");
+            System.out.print("Estado");
+                    
+            while (it.hasNext()){
+                transaccion=it.next();
+                System.out.print(transaccion.getTransaccionid()+"\t");
+                System.out.print(transaccion.getLibroidOrigen()+"\t");
+                System.out.print(transaccion.getLibroidDestino()+"\t");
+                System.out.println(transaccion.getEstado());
+                
+            }
+        }
+        catch(NoResultException e){
+            System.out.println("No hay datos");
+        } 
+    }
+    public static void inicializaFactory(){
+        emf = Persistence.createEntityManagerFactory ("com.mycompany_Equipo3_jar_1.0-SNAPSHOTPU");
+        em = emf.createEntityManager();
+    }
+    
+    
+    public static void main(String[] args){
+        inicializaFactory();
+        System.out.println("hola");
+        em.close();
+        emf.close();
+    }
     
 }
